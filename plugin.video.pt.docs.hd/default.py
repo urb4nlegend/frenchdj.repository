89c22ebd23981a772edaeef4f7c01e159f2b97bd
frenchdj <frenchdj@netcabo.pt>
2016-05-19 11:39:56 +0100

    Addon PT DocS HD 2.1

--- plugin.video.pt.docs.hd/default.py
+++ plugin.video.pt.docs.hd/default.py
@@ -35,6 +35,7 @@ icon12 = 'special://home/addons/plugin.video.pt.docs.hd/resources/icon12.png'
 
 
 
+
 def run():
     plugintools.log("pt.docs.hd.run")
     params = plugintools.get_params()
@@ -49,20 +50,17 @@ def main_list(params):
 	plugintools.add_item(title = "[ Acredite ou não ]", url = base + "channel/UCunqx90PNIv30uOgn0m4CTg/", thumbnail = iconAA, fanart = fan01, folder = True)
 	plugintools.add_item(title = "[ Pré-História - Origens ]", url = base + "playlist/PLAg4T2da6qWyDld8XLVstbH4ATeId6d6E/", thumbnail = icon01, fanart = fan01, folder = True)
 	plugintools.add_item(title = "[ Civilizações - Impérios ]", url = base + "playlist/PLAg4T2da6qWx4B6j1zPpgbyI-ROEzVeDZ/", thumbnail = icon02, fanart = fan01, folder = True)
-	plugintools.add_item(title = "[ Terremotos - Impactos ]", url = base + "playlist/PLAg4T2da6qWz9L4bxdK0jg4d01r50eM6P/", thumbnail = icon03, fanart = fan01, folder = True)
-	plugintools.add_item(title = "[ Futuro - Teorias ]", url = base + "playlist/PLAg4T2da6qWwvAWWAcD4Pb-6sTrmKiOyf/", thumbnail = icon04, fanart = fan01, folder = True)
-	plugintools.add_item(title = "[ Informática - Jogos ]", url = base + "playlist/PLAg4T2da6qWxPRExPKACw5RP3GWWYIkDJ/", thumbnail = icon05, fanart = fan01, folder = True)	
-	plugintools.add_item(title = "[ Tendências - Paranóias ]", url = base + "playlist/PLAg4T2da6qWxSswYqS6gi4XYZ3qlz5Azo/", thumbnail = icon06, fanart = fan01, folder = True)
-	plugintools.add_item(title = "[ Gangues - Prisões ]", url = base + "playlist/PLAg4T2da6qWxPpEjq9KWuGe6vF-DWbAiE/", thumbnail = icon07, fanart = fan01, folder = True)
-	plugintools.add_item(title = "[ Biografias - Eventos ]", url = base + "playlist/PLAg4T2da6qWzUkstE50-E3rRnLCW9nX2Q/", thumbnail = icon08, fanart = fan01, folder = True)
-	plugintools.add_item(title = "[ Cultos - Seitas ]", url = base + "playlist/PLAg4T2da6qWxPpEjq9KWuGe6vF-DWbAiE/", thumbnail = icon09, fanart = fan01, folder = True)
-	plugintools.add_item(title = "[ Cosmos - Planetas ]", url = base + "playlist/PLAg4T2da6qWzckdBwDHweyF6hqds1NtAL/" , thumbnail = icon10, fanart = fan01, folder = True)	
-	plugintools.add_item(title = "[ Vários ]", url = base + "playlist/PLAg4T2da6qWyEKYtDwim653ZYa7PYueK9/", thumbnail = icon11, fanart = fan01, folder = True)
-	plugintools.add_item(title = "[ Era uma Vez ]", url = base + "playlist/PLAg4T2da6qWwLIP_kqJn5WIvZeqkg3aJ5/", thumbnail = icon12, fanart = fan01, folder = True)
+	plugintools.add_item(title = "[ O Povo - A Terra ]", url = base + "playlist/PLAg4T2da6qWzR4wzIK8r9H8S_kR1PIvDX/", thumbnail = icon03, fanart = fan01, folder = True)
+	plugintools.add_item(title = "[ Terremotos - Impactos ]", url = base + "playlist/PLAg4T2da6qWz9L4bxdK0jg4d01r50eM6P/", thumbnail = icon04, fanart = fan01, folder = True)
+	plugintools.add_item(title = "[ Futuro - Teorias ]", url = base + "playlist/PLAg4T2da6qWwvAWWAcD4Pb-6sTrmKiOyf/", thumbnail = icon05, fanart = fan01, folder = True)
+	plugintools.add_item(title = "[ Informática - Jogos ]", url = base + "playlist/PLAg4T2da6qWxPRExPKACw5RP3GWWYIkDJ/", thumbnail = icon06, fanart = fan01, folder = True)	
+	plugintools.add_item(title = "[ Tendências - Paranóias ]", url = base + "playlist/PLAg4T2da6qWxSswYqS6gi4XYZ3qlz5Azo/", thumbnail = icon07, fanart = fan01, folder = True)
+	plugintools.add_item(title = "[ Gangues - Prisões ]", url = base + "playlist/PLAg4T2da6qWxPpEjq9KWuGe6vF-DWbAiE/", thumbnail = icon08, fanart = fan01, folder = True)
+	plugintools.add_item(title = "[ Biografias - Eventos ]", url = base + "playlist/PLAg4T2da6qWzUkstE50-E3rRnLCW9nX2Q/", thumbnail = icon09, fanart = fan01, folder = True)
+	plugintools.add_item(title = "[ Cultos - Seitas ]", url = base + "playlist/PLAg4T2da6qWxPpEjq9KWuGe6vF-DWbAiE/", thumbnail = icon10, fanart = fan01, folder = True)
+	plugintools.add_item(title = "[ Cosmos - Planetas ]", url = base + "playlist/PLAg4T2da6qWzckdBwDHweyF6hqds1NtAL/" , thumbnail = icon11, fanart = fan01, folder = True)	
+	plugintools.add_item(title = "[ Vários ]", url = base + "playlist/PLAg4T2da6qWyEKYtDwim653ZYa7PYueK9/", thumbnail = icon12, fanart = fan01, folder = True)
 	plugintools.add_item(title = "[ Revolução Ciêntifica ]", url = base + "channel/UCPLBXvobTRvDjPy41y9hG8g/", thumbnail = iconAB, fanart = fan01, folder = True)
-	
-
-	
 	xbmcplugin.setContent(int(sys.argv[1]), 'movies')
 	xbmc.executebuiltin('Container.SetViewMode(500)')
 	
