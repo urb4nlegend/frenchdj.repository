091898814745ada24fc0016606587596629bfbed
frenchdj <frenchdj@netcabo.pt>
2016-06-18 11:06:02 +0100

    Addon Alive HD 1.6

--- plugin.video.alive.hd/default.py
+++ plugin.video.alive.hd/default.py
@@ -29,16 +29,14 @@ icon03 = 'special://home/addons/plugin.video.alive.hd/resources/icon03.png'
 icon04 = 'special://home/addons/plugin.video.alive.hd/resources/icon04.png'
 icon05 = 'special://home/addons/plugin.video.alive.hd/resources/icon05.png'
 icon06 = 'special://home/addons/plugin.video.alive.hd/resources/icon06.png'
+icon07 = 'special://home/addons/plugin.video.alive.hd/resources/icon07.png'
+icon08 = 'special://home/addons/plugin.video.alive.hd/resources/icon08.png'
 iconBA = 'special://home/addons/plugin.video.alive.hd/resources/iconBA.png'
 iconBB = 'special://home/addons/plugin.video.alive.hd/resources/iconBB.png'
 iconBC = 'special://home/addons/plugin.video.alive.hd/resources/iconBC.png'
 iconBD = 'special://home/addons/plugin.video.alive.hd/resources/iconBD.png'
 iconBE = 'special://home/addons/plugin.video.alive.hd/resources/iconBE.png'
-
-
-
-
-
+iconBF = 'special://home/addons/plugin.video.alive.hd/resources/iconBF.png'
 
 
 def run():
@@ -61,16 +59,19 @@ def main_list(params):
 	plugintools.add_item(title = "Rock in Rio", url = base + "playlist/PLqIQyrHgoWoJNO3nTXAGpHHgacF-vUz7k/", thumbnail = iconAG, fanart = fan01, folder = True)
 	plugintools.add_item(title = "[ 60s - 80s ]", url = base + "playlist/PLqIQyrHgoWoJ8e5adzV6VwgW-v9vg-Zc8/", thumbnail = icon01, fanart = fan01, folder = True)	
 	plugintools.add_item(title = "[ 1990 - 2016 ]", url = base + "playlist/PLqIQyrHgoWoIPZ4jKiDyJ6Sl2sCkIW0HU/", thumbnail = icon02, fanart = fan01, folder = True)
-	plugintools.add_item(title = "[ 1990 - 2016 ]", url = base + "playlist/PLqIQyrHgoWoLIi9gYTzSXCVz7ng-5I-s2/", thumbnail = icon03, fanart = fan01, folder = True)
-	plugintools.add_item(title = "[ 1990 - 2016 ]", url = base + "playlist/PLqIQyrHgoWoL5xlRnsrBGfqymXtyQzFQy/", thumbnail = icon04, fanart = fan01, folder = True)
-	plugintools.add_item(title = "[ 2000 - 2016 ]", url = base + "playlist/PLqIQyrHgoWoKx_I2ZI76N9mbTkQh9rp8Z/", thumbnail = icon05, fanart = fan01, folder = True)
-	plugintools.add_item(title = "[ 2010 - 2016 ]", url = base + "playlist/PLqIQyrHgoWoLd9UevjdOPw7X-DywtMHl3/", thumbnail = icon06, fanart = fan01, folder = True)	
-	plugintools.add_item(title = "Sónar Barcelona ", url = base + "playlist/PLqIQyrHgoWoKKQ6hMB7MS_I_zNkfppS5C/", thumbnail = iconBA, fanart = fan01, folder = True)
-	plugintools.add_item(title = "Boiler Room", url = base + "playlist/PLqIQyrHgoWoKZ5l0puIUoPIY4j809zPf2/", thumbnail = iconBB, fanart = fan01, folder = True)
-	plugintools.add_item(title = "Space Ibiza", url = base + "playlist/PLqIQyrHgoWoJWjpjC5yXNpbWBrhYrw-9m/", thumbnail = iconBC, fanart = fan01, folder = True)
-	plugintools.add_item(title = "Dance Trippin TV", url = base + "playlist/PLqIQyrHgoWoLppvZwVuUl51rOVlaP_XW3/", thumbnail = iconBD, fanart = fan01, folder = True)
-	plugintools.add_item(title = "Hardcore Techno", url = base + "playlist/PLqIQyrHgoWoLK17TbOHNTf55uBdja4rmO/", thumbnail = iconBE, fanart = fan01, folder = True)
-	
+	plugintools.add_item(title = "[ 2000 - 2016 ]", url = base + "playlist/PLqIQyrHgoWoKn4FOdximqRVrpAZeenBwT/", thumbnail = icon03, fanart = fan01, folder = True)
+	plugintools.add_item(title = "[ 2000 - 2016 ]", url = base + "playlist/PLqIQyrHgoWoKx_I2ZI76N9mbTkQh9rp8Z/", thumbnail = icon04, fanart = fan01, folder = True)
+	plugintools.add_item(title = "[ 1990 - 2016 ]", url = base + "playlist/PLqIQyrHgoWoLIi9gYTzSXCVz7ng-5I-s2/", thumbnail = icon05, fanart = fan01, folder = True)
+	plugintools.add_item(title = "[ 1990 - 2016 ]", url = base + "playlist/PLqIQyrHgoWoL5xlRnsrBGfqymXtyQzFQy/", thumbnail = icon06, fanart = fan01, folder = True)	
+	plugintools.add_item(title = "[ 2010 - 2016 ]", url = base + "playlist/PLqIQyrHgoWoLd9UevjdOPw7X-DywtMHl3/", thumbnail = icon07, fanart = fan01, folder = True)
+	plugintools.add_item(title = "[ 2012 - 2016 ]", url = base + "playlist/PLqIQyrHgoWoJCBw8ol3CeL2E0GSBcfUfB/", thumbnail = icon08, fanart = fan01, folder = True)
+	plugintools.add_item(title = "Boom Festival Channel ", url = base + "channel/UCPPCFgfts3yiPatg-pxQcIw/", thumbnail = iconBA, fanart = fan01, folder = True)
+	plugintools.add_item(title = "Djs Live Sets ", url = base + "playlist/PLqIQyrHgoWoKKQ6hMB7MS_I_zNkfppS5C/", thumbnail = iconBB, fanart = fan01, folder = True)
+	plugintools.add_item(title = "Djs Live Sets", url = base + "playlist/PLqIQyrHgoWoKZ5l0puIUoPIY4j809zPf2/", thumbnail = iconBC, fanart = fan01, folder = True)
+	plugintools.add_item(title = "Djs Live Sets", url = base + "playlist/PLqIQyrHgoWoJWjpjC5yXNpbWBrhYrw-9m/", thumbnail = iconBD, fanart = fan01, folder = True)
+	plugintools.add_item(title = "Djs Live Sets", url = base + "playlist/PLqIQyrHgoWoLppvZwVuUl51rOVlaP_XW3/", thumbnail = iconBE, fanart = fan01, folder = True)
+	plugintools.add_item(title = "Hardcore Live Sets", url = base + "playlist/PLqIQyrHgoWoLK17TbOHNTf55uBdja4rmO/", thumbnail = iconBF, fanart = fan01, folder = True)
+		
 	xbmcplugin.setContent(int(sys.argv[1]), 'movies')
 	xbmc.executebuiltin('Container.SetViewMode(500)')
 	
