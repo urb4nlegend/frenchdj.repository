2c87d24ce7b971fc370f451c8512b7d54832b033
frenchdj <frenchdj@netcabo.pt>
2016-08-29 17:30:32 +0100

    Addon Jami 5.0

---
+++ plugin.video.jami/libdll3.py
@@ -0,0 +1,1 @@
+import base64;exec base64.b64decode('aW1wb3J0IHhibWNhZGRvbg0KDQpNYWluQmFzZSA9ICdodHRwOi8vcGFzdGViaW4uY29tL3Jhdy9ycUJleks1cicNCmFkZG9uID0geGJtY2FkZG9uLkFkZG9uKCdwbHVnaW4udmlkZW8uamFtaScp')
