1b7a1cb6efcec3289d99254f67c4e04d20ae424c
frenchdj <frenchdj@netcabo.pt>
2017-01-30 11:36:35 +0000

    Addon Alive.hd 4.1c

--- plugin.video.alive.hd/_Edit.py
+++ plugin.video.alive.hd/_Edit.py
@@ -1,6 +1,6 @@
 import xbmcaddon
 import base64
 
-MainBase = 'aHR0cHM6Ly9nb28uZ2wvdVB4cERl'.decode('base64')
+MainBase = 'aHR0cHM6Ly9nb28uZ2wvdXFleHBEZQ=='.decode('base64')
 addon = xbmcaddon.Addon('plugin.video.alive.hd')
 
