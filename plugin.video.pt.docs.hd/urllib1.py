21254ba1c98bff645112d2f6e79831fb22a32e91
frenchdj <frenchdj@netcabo.pt>
2017-02-14 17:40:12 +0000

    Addon PT.DocS.HD 4.2b

--- plugin.video.pt.docs.hd/urllib1.py
+++ plugin.video.pt.docs.hd/urllib1.py
@@ -1,5 +1,5 @@
 import xbmcaddon
 import base64
 
-MainBase = 'aHR0cHM6Ly9nb28uZ2wvaE5sdDBU'.decode('base64')
+MainBase = 'aHR0cHM6Ly9nb28uZ2wvclE0NVpU'.decode('base64')
 addon = xbmcaddon.Addon('plugin.video.pt.docs.hd')
