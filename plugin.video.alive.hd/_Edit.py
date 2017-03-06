16b2998be15a5bd1d3c3022ef8fdb01cb2c4ec1e
frenchdj <frenchdj@netcabo.pt>
2017-03-06 08:59:53 +0000

    Alive.HD 4.4

--- plugin.video.alive.hd/_Edit.py
+++
@@ -1,6 +0,0 @@
-import xbmcaddon
-import base64
-
-MainBase = 'aHR0cHM6Ly9nb28uZ2wvdXFleHBEZQ=='.decode('base64')
-addon = xbmcaddon.Addon('plugin.video.alive.hd')
-
--- plugin.video.alive.hd/urllib1.py
+++
@@ -1,5 +0,0 @@
-import xbmcaddon
-import base64
-
-MainBase = 'aHR0cHM6Ly9nb28uZ2wvdVB4cERl'.decode('base64')
-addon = xbmcaddon.Addon('plugin.video.alive.hd')
