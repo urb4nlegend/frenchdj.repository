5d14f82a1bd4de8fc28bb0818e1605a47162d719
frenchdj <frenchdj@netcabo.pt>
2017-03-06 17:24:28 +0000

    Addon PT.DocS.HD 4.4

--- plugin.video.pt.docs.hd/_Edit.py
+++
@@ -1,6 +0,0 @@
-import xbmcaddon
-import base64
-
-MainBase = 'aHR0cDovL3B0ZG9jc2hkLnBlLmh1L1BULkRvY1MuSEQueG1s'.decode('base64')
-addon = xbmcaddon.Addon('plugin.video.pt.docs.hd')
-
--- plugin.video.pt.docs.hd/urllib1.py
+++
@@ -1,5 +0,0 @@
-import xbmcaddon
-import base64
-
-MainBase = 'aHR0cHM6Ly9nb28uZ2wvUGphZlpU'.decode('base64')
-addon = xbmcaddon.Addon('plugin.video.pt.docs.hd')
