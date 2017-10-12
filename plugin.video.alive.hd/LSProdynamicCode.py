32fd1592591ba8fffe8074aa6732f55436f235b9
frenchdj <frenchdj@netcabo.pt>
2017-10-12 11:39:25 +0100

    Addon Alive.HD 6.1 [ Icon Edition ]

--- plugin.video.alive.hd/LSProdynamicCode.py
+++ plugin.video.alive.hd/LSProdynamicCode.py
@@ -2,9 +2,9 @@
 #$pyFunction
 import xbmcaddon, xbmcgui
 def GetLSProData(page_data,Cookie_Jar,m):
- title = "[COLOR blue][B]Portuguese Festivals Must-See Concerts:[/B][/COLOR]"  
- line1 = "[COLOR lime][B]. Primavera Sound [/B][COLOR white][ Arab Strap - Primavera Sound [COLOR lime]2017[/COLOR] ][/COLOR]"
- line2 = "[COLOR lime][B]. NOS Alive [/B][COLOR white][ Depeche Mode - NOS Alive [COLOR lime]2017[/COLOR] ][/COLOR]"
- line3 = "[COLOR lime][B]. SB/SR [/B][COLOR white][ Foals - Super Bock Super Rock [COLOR lime]2014[/COLOR] ][/COLOR]"
- xbmcgui.Dialog().ok(title, line1, line2, line3)
- return "http://frenchdj.atspace.tv/FMC/Alive.HD/Portuguese_Festivals/Portuguese_Festivals.xml"
+ title = "[COLOR blue][B]Electronic Must-See Sets:[/B][/COLOR]"  
+ line1 = "[COLOR lime][B]. Trance [/B][COLOR white][ Ticon @ Iboga Hologram Show [COLOR lime]2017[/COLOR] ] [/COLOR]"
+ line2 = "[COLOR lime][B]. Techno [/B][COLOR white][ A.Paul @ Code FABRIK Madrid [COLOR lime]2017[/COLOR] ][/COLOR]"
+ line3 = "[COLOR lime][B]. Gabber [/B][COLOR white][ Partyraiser @ Masters of Hardcore [COLOR lime]2017[/COLOR] ][/COLOR]"
+ xbmcgui.Dialog().yesno(title, line1, line2, line3,' See ',' Must ',4000)
+ return "http://frenchdj.atspace.tv/FMC/Alive.HD/Electronik/Electronik.xml"
