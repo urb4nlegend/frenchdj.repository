023a2396aa1f0ceb76881bf5f390d10668795520
frenchdj <frenchdj@netcabo.pt>
2018-02-20 10:50:23 +0000

    Addon Documented.HD 5.0a

--- plugin.video.documented.hd/default.py
+++ plugin.video.documented.hd/default.py
@@ -99,14 +99,14 @@ def makeRequest(url, headers=None):
                 xbmc.executebuiltin("XBMC.Notification(documented.hd, server error. - "+str(e.reason)+",10000,"+icon+")")
 				
 def SKindex():
-    addDir('[B][COLOR lime]| Doc[COLOR white].[COLOR lime]HD Favourites[/COLOR][/B]','[B][COLOR lime]| Doc[COLOR white].[COLOR lime]HD Favourites[/COLOR][/B]',4,'https://i.imgur.com/gQteu00.png',FANART,'','','','')
+    addDir('[B][COLOR lime]| Favourites[/COLOR][/B]','[B][COLOR lime]| Favourites[/COLOR][/B]',4,'https://i.imgur.com/gQteu00.png',FANART,'','','','')
     getData(Base,FANART)
     xbmcplugin.endOfDirectory(int(sys.argv[1]))
 				
 def getSources():
         try:
             if os.path.exists(favorites) == True:
-                addDir('[B][COLOR lime]| Doc[COLOR white].[COLOR lime]HD Favourites[/COLOR][/B]','[B][COLOR lime]| Doc[COLOR white].[COLOR lime]HD Favourites[/COLOR][/B]',4,'https://i.imgur.com/gQteu00.png' , os.path.join(home, 'fanart.gif'),FANART,'','','','')
+                addDir('[B][COLOR lime]| Favourites[/COLOR][/B]','[B][COLOR lime]| Favourites[/COLOR][/B]',4,'https://i.imgur.com/gQteu00.png' , os.path.join(home, 'fanart.gif'),FANART,'','','','')
             if addon.getSetting("browse_xml_database") == "true":
                 addDir('XML Database','http://xbmcplus.xb.funpic.de/www-data/filesystem/',15,icon,FANART,'','','','')
             if addon.getSetting("browse_community") == "true":
